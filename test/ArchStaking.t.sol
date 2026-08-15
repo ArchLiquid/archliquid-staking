02801d5d1cb76d34bd5ce1a045e9b63ea9ccb41c
Filip Smith <filiptsmith@gmail.com>
2026-08-15 23:07:06 +0300

    Test staking token accounting edges

--- test/ArchStaking.t.sol
+++ test/ArchStaking.t.sol
@@ -5,7 +5,7 @@ import {Test} from "forge-std/Test.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {ArchStakingFactory} from "../src/ArchStakingFactory.sol";
 import {ArchStakingPool} from "../src/ArchStakingPool.sol";
-import {MockERC20} from "./mocks/Mocks.sol";
+import {MockERC20, MockERC20Decimals, MockFeeOnTransferERC20} from "./mocks/Mocks.sol";
 
 contract ArchStakingTest is Test {
     uint256 constant DEPLOY_FEE = 0.05 ether;
@@ -166,5 +166,100 @@ contract ArchStakingTest is Test {
         vm.prank(alice);
         pool.getReward();
         assertLe(rewardTok.balanceOf(alice), 44_100e18);
+    }
+
+    function test_sixDecimalStakeAndEightDecimalRewardAccounting() public {
+        _assertDecimalLifecycle(6, 8);
+    }
+
+    function test_eightDecimalStakeAndEighteenDecimalRewardAccounting() public {
+        _assertDecimalLifecycle(8, 18);
+    }
+
+    function test_eighteenDecimalStakeAndSixDecimalRewardAccounting() public {
+        _assertDecimalLifecycle(18, 6);
+    }
+
+    function test_feeOnTransferStakeCreditsOnlyTokensReceived() public {
+        MockFeeOnTransferERC20 taxedStake = new MockFeeOnTransferERC20("Taxed stake", "TSTK", 200);
+        ArchStakingPool taxedPool = _createPool(IERC20(address(taxedStake)), IERC20(address(rewardTok)));
+
+        taxedStake.mint(alice, 100e18);
+        vm.startPrank(alice);
+        taxedStake.approve(address(taxedPool), type(uint256).max);
+        taxedPool.stake(100e18);
+        vm.stopPrank();
+
+        assertEq(taxedStake.balanceOf(address(taxedPool)), 98e18);
+        assertEq(taxedPool.balanceOf(alice), 98e18);
+        assertEq(taxedPool.totalSupply(), 98e18);
+
+        vm.prank(alice);
+        taxedPool.withdraw(98e18);
+
+        assertEq(taxedPool.balanceOf(alice), 0);
+        assertEq(taxedPool.totalSupply(), 0);
+        assertEq(taxedStake.balanceOf(address(taxedPool)), 0);
+        assertEq(taxedStake.balanceOf(alice), 96_040_000_000_000_000_000);
+    }
+
+    function test_feeOnTransferRewardsRemainFullyBacked() public {
+        MockFeeOnTransferERC20 taxedReward = new MockFeeOnTransferERC20("Taxed reward", "TRWD", 1_000);
+        ArchStakingPool taxedPool = _createPool(IERC20(address(stakeTok)), IERC20(address(taxedReward)));
+
+        stakeTok.mint(alice, 100e18);
+        taxedReward.mint(operator, 1_000e18);
+        vm.prank(alice);
+        stakeTok.approve(address(taxedPool), type(uint256).max);
+        vm.prank(alice);
+        taxedPool.stake(100e18);
+        vm.prank(operator);
+        taxedReward.approve(address(taxedPool), type(uint256).max);
+        vm.prank(operator);
+        taxedPool.notifyRewardAmount(1_000e18);
+
+        // The pool receives 900, attempts an 18-token protocol fee, and the
+        // reward schedule is backed by the exact 882 tokens left in custody.
+        assertEq(taxedReward.balanceOf(address(taxedPool)), 882e18);
+        assertEq(taxedReward.balanceOf(treasury), 16_200_000_000_000_000_000);
+        assertLe(taxedPool.rewardRate() * DURATION, 882e18);
+
+        vm.warp(block.timestamp + DURATION);
+        uint256 earned = taxedPool.earned(alice);
+        assertLe(earned, 882e18);
+        vm.prank(alice);
+        taxedPool.getReward();
+        assertLe(taxedReward.balanceOf(alice), earned);
+        assertLe(taxedReward.balanceOf(address(taxedPool)), 882e18 - earned);
+    }
+
+    function _assertDecimalLifecycle(uint8 stakeDecimals, uint8 rewardDecimals) internal {
+        MockERC20Decimals customStake = new MockERC20Decimals("Custom stake", "CSTK", stakeDecimals);
+        MockERC20Decimals customReward = new MockERC20Decimals("Custom reward", "CRWD", rewardDecimals);
+        ArchStakingPool customPool = _createPool(IERC20(address(customStake)), IERC20(address(customReward)));
+
+        uint256 stakeUnit = 10 ** uint256(stakeDecimals);
+        uint256 rewardUnit = 10 ** uint256(rewardDecimals);
+        customStake.mint(alice, 100 * stakeUnit);
+        customReward.mint(operator, 30_000 * rewardUnit);
+
+        vm.prank(alice);
+        customStake.approve(address(customPool), type(uint256).max);
+        vm.prank(alice);
+        customPool.stake(100 * stakeUnit);
+        vm.prank(operator);
+        customReward.approve(address(customPool), type(uint256).max);
+        vm.prank(operator);
+        customPool.notifyRewardAmount(30_000 * rewardUnit);
+
+        assertEq(customPool.balanceOf(alice), 100 * stakeUnit);
+        assertEq(customPool.totalSupply(), 100 * stakeUnit);
+        vm.warp(block.timestamp + DURATION);
+        assertApproxEqRel(customPool.earned(alice), 29_400 * rewardUnit, 1e15);
+    }
+
+    function _createPool(IERC20 stakingToken, IERC20 rewardsToken) internal returns (ArchStakingPool created) {
+        vm.prank(operator);
+        created = ArchStakingPool(factory.createPool{value: DEPLOY_FEE}(stakingToken, rewardsToken, DURATION, 0));
     }
 }
--- test/mocks/Mocks.sol
+++ test/mocks/Mocks.sol
@@ -10,3 +10,43 @@ contract MockERC20 is ERC20 {
         _mint(to, amount);
     }
 }
+
+contract MockERC20Decimals is ERC20 {
+    uint8 private immutable _tokenDecimals;
+
+    constructor(string memory name_, string memory symbol_, uint8 decimals_) ERC20(name_, symbol_) {
+        _tokenDecimals = decimals_;
+    }
+
+    function decimals() public view override returns (uint8) {
+        return _tokenDecimals;
+    }
+
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+}
+
+contract MockFeeOnTransferERC20 is ERC20 {
+    uint16 public immutable feeBps;
+
+    constructor(string memory name_, string memory symbol_, uint16 feeBps_) ERC20(name_, symbol_) {
+        require(feeBps_ <= 1_000, "mock: fee too high");
+        feeBps = feeBps_;
+    }
+
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+
+    function _update(address from, address to, uint256 amount) internal override {
+        if (from == address(0) || to == address(0) || feeBps == 0) {
+            super._update(from, to, amount);
+            return;
+        }
+
+        uint256 fee = (amount * feeBps) / 10_000;
+        super._update(from, address(0), fee);
+        super._update(from, to, amount - fee);
+    }
+}
